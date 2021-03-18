0f300d6e2a46820f6208007339302a8dbe9ea6ae
Darius <flocadariusandrei@gmail.com>
2021-03-18 21:18:01 +0200

    File Parser

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+npm i -g
+cp plugin-info.json ~/.dx-platform/plugins/file-parser/plugin-info.json
---
+++ index.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+class Component {
+    constructor(fullyQualifiedName, files) {
+        this.fullyQualifiedName = fullyQualifiedName
+        this.files = files
+    }
+}
+
+const yargs = require("yargs");
+const fs = require('fs');
+
+const options = yargs
+    .usage("Usage: file-parser --of <outputFile> --ftg <filesToGroup>")
+    .option("of", {
+        alias: "outputFile",
+        describe: "Output file to write your results to",
+        type: "string",
+        demandOption: true
+    })
+    .option("ftg", {
+        alias: "filesToGroup",
+        describe: "A file containing all files the plugin should group into components (one file per line)",
+        type: "string",
+        demandOption: true
+    })
+    .argv;
+
+const components = []
+const defaultComponent = new Component("@", [])
+const controllersComponent = new Component("controllers", [])
+const servicesComponent = new Component("services", [])
+const repositoriesComponent = new Component("repositories", [])
+
+const filesToGroup = options.ftg
+
+const allFiles = fs.readFileSync(filesToGroup, {encoding: 'UTF-8'}).toString().split("\n").filter(file => file.length !== 0)
+
+allFiles.forEach(currentFile => {
+    fs.readFile(currentFile, "UTF-8", function (err, contents) {
+        if (contents.includes("@RestController")) {
+            controllersComponent.files.push(currentFile)
+        } else if (contents.includes("@Service")) {
+            servicesComponent.files.push(currentFile)
+        } else if (contents.includes("JpaRepository")) {
+            repositoriesComponent.files.push(currentFile)
+        } else {
+            defaultComponent.files.push(currentFile)
+        }
+    })
+})
+
+components.push(controllersComponent)
+components.push(servicesComponent)
+components.push(repositoriesComponent)
+components.push(defaultComponent)
+
+
+setTimeout(function () {
+    fs.writeFileSync(options.of, JSON.stringify(components))
+}, 1000)
